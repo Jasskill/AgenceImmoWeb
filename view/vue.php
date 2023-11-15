473e7f249a3e329f6d789814af862742721ecb28
t3lss <erwann.savea@gmail.com>
2023-11-15 17:15:56 +0100

    on croise les doigt mdrr

--- view/vue.php
+++ view/vue.php
@@ -3,62 +3,80 @@ class vue{
     public function entete(){
         echo '
         <!DOCTYPE html>
-        <html lang=\"en\">
+        <html lang="en">
         <head>
-            <meta charset=\"UTF-8\">
-            <meta http-equiv=\"X-UA-Compatible\" content=\"IE=edge\">
-            <meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0\">
-            <link rel=\"stylesheet\" href=\"css/bootstrap.min.css\">
+            <meta charset="UTF-8">
+            <meta http-equiv="X-UA-Compatible" content="IE=edge">
+            <meta name="viewport" content="width=device-width, initial-scale=1.0">
+            <link rel="stylesheet" href="css/bootstrap.min.css">
             <title>Document</title>
         </head>
         <body>
-        <nav class="navbar bg-body-tertiary fixed-top">
-        <div class="container-fluid">
-          <a class="navbar-brand" href="#">Agence Immo Web</a>
-          <button class="navbar-toggler" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasNavbar" aria-controls="offcanvasNavbar" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-          </button>
-          <div class="offcanvas offcanvas-end" tabindex="-1" id="offcanvasNavbar" aria-labelledby="offcanvasNavbarLabel">
-            <div class="offcanvas-header">
-              <h5 class="offcanvas-title" id="offcanvasNavbarLabel">Offcanvas</h5>
-              <button type="button" class="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
-            </div>
-            <div class="offcanvas-body">
-              <ul class="navbar-nav justify-content-end flex-grow-1 pe-3">
-                <li class="nav-item">
-                  <a class="nav-link active" aria-current="page" href="#">Home</a>
-                </li>
-                <li class="nav-item">
-                  <a class="nav-link" href="#">Link</a>
-                </li>
-                <li class="nav-item dropdown">
-                  <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                    Dropdown
-                  </a>
-                  <ul class="dropdown-menu">
-                    <li><a class="dropdown-item" href="#">Action</a></li>
-                    <li><a class="dropdown-item" href="#">Another action</a></li>
-                    <li>
-                      <hr class="dropdown-divider">
+        <nav class="navbar navbar-expand-lg navbar-dark bg-dark fixed-top">
+        <div class="container">
+            <a class="navbar-brand" href="#">Agence Immo Web</a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
+                aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNav">
+                <ul class="navbar-nav ml-auto">
+                    <li class="nav-item">
+                        <a class="nav-link" href="index.php?action=afficherPageAccueil">Annonces</a>
                     </li>
-                    <li><a class="dropdown-item" href="#">Something else here</a></li>
-                  </ul>
-                </li>
-              </ul>
-              <form class="d-flex mt-3" role="search">
-                <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
-                <button class="btn btn-outline-success" type="submit">Search</button>
-              </form>
-            </div>
-          </div>
-        </div>
-      </nav>';
+                    <li class="nav-item">
+                        <a class="nav-link" href="index.php?action=afficherPageCommentaires">Connexion</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="index.php?action=afficherPageArticle&id=1">Inscription</a>
+                    </li>
+                </ul>
+                <div class="alig-end">
+                <form class="d-flex" role="search">
+        <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+        <button class="btn btn-outline-success" type="submit">Search</button>
+      </form>
+                </div>';
     }
 
+    public function enteteLocataireCo(){
+        echo '
+        <!DOCTYPE html>
+        <html lang="en">
+        <head>
+            <meta charset="UTF-8">
+            <meta http-equiv="X-UA-Compatible" content="IE=edge">
+            <meta name="viewport" content="width=device-width, initial-scale=1.0">
+            <link rel="stylesheet" href="css/bootstrap.min.css">
+            <title>Document</title>
+        </head>
+        <body>
+        <nav class="navbar navbar-expand-lg navbar-dark bg-dark fixed-top">
+        <div class="container">
+            <a class="navbar-brand" href="#">Agence Immo Web</a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
+                aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNav">
+                <ul class="navbar-nav ml-auto">
+                    <li class="nav-item">
+                        <a class="nav-link" href="index.php?action=afficherPageAccueil">Annonces</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="index.php?action=afficherPageCommentaires">Connexion</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="index.php?action=afficherPageArticle&id=1">Inscription</a>
+                    </li>
+                </ul>';
+    }
+    }
     public function fin(){
         echo "
         </body>
         </html>"
     }
-}
+
+
 ?>
