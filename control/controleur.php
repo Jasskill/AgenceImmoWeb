9022dbe41d1a6d633baad9c0a14db218e6d09a0f
ljacquemot <luckas28@gmx.fr>
2023-11-15 14:53:07 +0100

    modif index, controleur, vue

---
+++ control/controleur.php
@@ -0,0 +1,7 @@
+<?php
+class controleur{
+    public function accueil() {
+		(new vue)->accueil();
+	}
+}
+?>
--- index.php
+++ index.php
@@ -1,1 +1,52 @@
-<?php?>
+<?php
+session_start();
+
+// Test de connexion à la base
+$config = parse_ini_file("config.ini");
+try {
+	$pdo = new \PDO("mysql:host=".$config["host"].";dbname=".$config["database"].";charset=utf8", $config["user"], $config["password"]);
+} catch(Exception $e) {
+	echo "<h1>Erreur de connexion à la base de données :</h1>";
+	echo $e->getMessage();
+	exit;
+}
+
+// Chargement des fichiers MVC
+require("control/controleur.php");
+require("view/vue.php");
+require("model/Utilisateur.php");
+
+//Routes
+if(isset($_GET["action"])){
+    switch($_GET["action"]){
+        case "accueil":
+            (new controleur)->accueil();
+            break;
+        case "connexion":
+            (new controleur)->connexion();
+            break;
+        case "inscription":
+            (new controleur)->inscription();
+            break;
+        case "acceesPropritaire":
+            (new controleur)->acceesPropritaire();
+            break;
+        case "recherche":
+            (new controleur)->recherche();
+            break;
+        case "ajoutDisponibilite":
+            (new controleur)->ajoutDisponibilite();
+            break;
+        case "demandeReservation":
+            (new controleur)->demandeReservation();
+            break;
+        default:
+            //route par default : erreur404
+            (new controleur)->erreur404():
+            break;
+    }
+}else{
+    // Pas d'action précisée = afficher l'accueil
+    (new controleur)->accueil();
+}
+?>
