a50e35f86567e1a09b2226d55d04d64d7d8a6bb4
t3lss <erwann.savea@gmail.com>
2023-11-22 15:11:43 +0100

    mercredi 22

--- view/vue.php
+++ view/vue.php
@@ -3,40 +3,45 @@ class vue{
     public function entete(){
         echo '
         <!DOCTYPE html>
-        <html lang="en">
-        <head>
-            <meta charset="UTF-8">
-            <meta http-equiv="X-UA-Compatible" content="IE=edge">
-            <meta name="viewport" content="width=device-width, initial-scale=1.0">
-            <link rel="stylesheet" href="css/bootstrap.min.css">
-            <title>Document</title>
-        </head>
-        <body>
+    <html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <!--<link rel="stylesheet" href="css/bootstrap.min.css">-->
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+        <title>Document</title>
+    </head>
+    <body>
         <nav class="navbar navbar-expand-lg navbar-dark bg-dark fixed-top">
-        <div class="container">
-            <a class="navbar-brand" href="#">Agence Immo Web</a>
-            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
-                aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
-            </button>
-            <div class="collapse navbar-collapse" id="navbarNav">
-                <ul class="navbar-nav ml-auto">
-                    <li class="nav-item">
-                        <a class="nav-link" href="index.php?action=afficherPageAccueil">Annonces</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="index.php?action=afficherPageCommentaires">Connexion</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="index.php?action=afficherPageArticle&id=1">Inscription</a>
-                    </li>
-                </ul>
-                <div class="alig-end">
-                <form class="d-flex" role="search">
-        <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
-        <button class="btn btn-outline-success" type="submit">Search</button>
-      </form>
-                </div>';
+            <div class="container">
+                <div class="row">
+                    <a class="navbar-brand" href="#">Agence Immo Web</a>
+                </div>
+                    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                        <span class="navbar-toggler-icon"></span>
+                    </button>
+                <div class="collapse navbar-collapse" id="navbarNav">
+                    <ul class="navbar-nav ms-auto">
+                        <li class="nav-item">
+                            <a class="nav-link" href="index.php?action=afficherPageAccueil">Annonces</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="index.php?action=afficherPageCommentaires">Connexion</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="index.php?action=afficherPageArticle&id=1">Inscription</a>
+                        </li>
+                    </ul>
+                    <form class="d-flex" role="search">
+                        <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+                        <button class="btn btn-outline-success" type="submit">Search</button>
+                    </form>
+                </div>
+            </div>
+        </nav>
+    </body>
+    </html>';
     }
     //Affichage connexion utilisateur
     public function connexion ($message = null){
